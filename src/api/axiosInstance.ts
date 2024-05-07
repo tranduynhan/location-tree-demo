e9c04467b05110a68de809cc51d17e8f8f433a2b
tranduynhan <tranduynhan92@gmail.com>
2024-05-07 21:06:29 +0700

    feat: [Config] check config base URL

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_URL;
+console.log(" BASE_URL -- ", BASE_URL);
 
 const httpService = axios.create({
   baseURL: BASE_URL ? BASE_URL : "https://663a1b561ae792804bee15db.mockapi.io",
